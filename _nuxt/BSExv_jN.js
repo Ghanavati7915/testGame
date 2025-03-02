04174ed0507685dfb0976343fb0bdde4337c11d6
Ahmad Ghanavati <Ahmad_ghanavati@yahoo.com>
2025-03-02 12:08:53 +0330

    Add files via upload

---
+++ _nuxt/BSExv_jN.js
@@ -0,0 +1,1 @@
+import{_ as s,v as t,x as o,I as c}from"./BxJXgdpn.js";const r={},a={class:"w-full"};function n(e,l){return o(),t("div",a,[c(e.$slots,"default")])}const f=s(r,[["render",n]]);export{f as default};
